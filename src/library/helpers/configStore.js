b0fc62be1e11213e73999e6474125bf3a811ee69
ertugrulsen <46446809+ertugrulsen@users.noreply.github.com>
2021-03-16 11:20:19 +0300

    Update configStore.js

--- src/library/helpers/configStore.js
+++ src/library/helpers/configStore.js
@@ -1,7 +1,7 @@
 const configStore = {
   settings: {
     localUrl:"http://localhost:52518/api/",
-    publishUrl: "http://104.154.140.175:5000/api/"
+    publishUrl: "http://104.154.140.175:4001/api/"
   }
 }
 
